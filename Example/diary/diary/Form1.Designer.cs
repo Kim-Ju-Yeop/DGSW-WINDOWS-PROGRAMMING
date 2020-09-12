637fdc093dd7836203dffa99d6c6d5d6be6dbc79
Kim-Ju-Yeop <kjy031104@gmail.com>
2020-09-12 11:37:50 +0900

    [ADD] Diary Example

---
+++ Example/diary/diary/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace diary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(12, 49);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(564, 334);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 401);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(483, 21);
+            this.textBox.TabIndex = 1;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(501, 401);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 2;
+            this.button.Text = "작성하기";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.insertData);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(501, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "새로고침";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.refreshData);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.listView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.refreshData);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Example/diary/diary/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace diary
+{
+    public partial class Form1 : Form
+    {
+        String connStr = "Server=10.80.163.37;Database=test;Uid=root;Pwd=tiger";
+        String id = "Heesong2";
+
+        public Form1()
+        {
+            InitializeComponent();
+            initListView();
+            getAllData();
+        }
+
+        public void initListView()
+        {
+            listView.View = View.Details;
+            listView.GridLines = true;
+
+            listView.Columns.Add("날짜");
+            listView.Columns.Add("일기");
+            listView.Columns.Add("아이디");
+        }
+
+        public void getAllData()
+        {
+            listView.Items.Clear();
+
+            using(MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                String sql = "SELECT * FROM diary WHERE id = '" + id + "'";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    String[] dataArr = { rdr["date"].ToString(), rdr["contents"].ToString(), rdr["id"].ToString() };
+                    ListViewItem listViewItem = new ListViewItem(dataArr);
+
+                    listView.Items.Add(listViewItem);
+                }
+            }
+        }
+
+        private void insertData(object sender, EventArgs e)
+        {
+            if (textBox.Text.Equals(""))
+            {
+                MessageBox.Show("내용을 정확히 입력하시기 바랍니다.");
+            } 
+            else
+            {
+                DateTime now = DateTime.Now;
+                String date = now.ToString("yyyy-MM-dd tt HH:mm:ss");
+                String content = textBox.Text.ToString();
+
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    String sql = "INSERT INTO diary VALUES(" + "'" + date + "'" + "," + "'" + content + "'" + "," + "'" + id + "'" + ");";
+
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("정상적으로 글 작성을 수행하였습니다.");
+                    textBox.Text = "";
+
+                    getAllData();
+                }
+            }
+        }
+
+        private void refreshData(object sender, EventArgs e)
+        {
+            MessageBox.Show("정상적으로 새로고침을 수행하였습니다.");
+            getAllData();
+        }
+    }
+}
